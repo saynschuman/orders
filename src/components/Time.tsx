aedf4e09258593fd5f017fb2eff24b78007da11e
vitaliy <vitaliy@heliotrope.digital>
2023-04-24 21:43:56 +0300

    time fixed

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -2,5 +2,9 @@ import { convertUnixTimestampToDate } from "@/utils";
 import { Box } from "@mui/system";
 
 export const Time = (props: { unix: number }) => {
-  return <Box sx={{ my: 1, color: "#999" }}>{convertUnixTimestampToDate(props.unix)}</Box>;
+  return (
+    <Box sx={{ my: 1, color: "#999" }}>
+      {convertUnixTimestampToDate(props.unix)}
+    </Box>
+  );
 };
