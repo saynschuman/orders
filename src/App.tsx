8ab7b80baff6e92f40dea1dd4f086c048001fbbb
vitaliy <vitaliy@heliotrope.digital>
2023-04-24 21:54:20 +0300

    scroll bottom added

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,35 @@ import { getOrders } from "./api";
 import { Order } from "./components/Order";
 import { classes } from "./styles";
 import { Box } from "@mui/material";
+import { useEffect, useRef } from "react";
 import { useQuery } from "react-query";
 
 function App() {
   const { data } = useQuery("orders", getOrders);
   const orders = data?.data?.result?.projects;
 
+  // Создаем ref для контейнера со списком элементов
+  const ordersContainerRef = useRef<HTMLDivElement>(null);
+
+  // Функция для прокрутки к последнему элементу
+  const scrollToBottom = () => {
+    const container = ordersContainerRef.current;
+    if (container) {
+      container.scrollTop = container.scrollHeight;
+    }
+  };
+
+  // Вызываем функцию scrollToBottom после загрузки данных
+  useEffect(() => {
+    if (orders?.length) {
+      scrollToBottom();
+    }
+  }, [orders]);
+
   return (
-    <Box sx={classes.wrapper}>
+    <Box sx={classes.wrapper} ref={ordersContainerRef}>
       {orders?.map((order) => (
-        <Order {...order} />
+        <Order {...order} key={order.id} />
       ))}
     </Box>
   );
--- src/styles.ts
+++ src/styles.ts
@@ -4,5 +4,7 @@ export const classes = {
     p: 2,
     maxWidth: 700,
     m: "auto",
+    height: "90vh",
+    overflow: "auto",
   },
 };
