0e35826ed4e1b4616dd0fa20ba6d086653f74865
vitaliy <vitaliy@heliotrope.digital>
2023-04-24 16:15:49 +0300

    prompt added

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -2,8 +2,14 @@ import { copyToClipboard } from "@/utils";
 import { Button } from "@mui/material";
 import { useState } from "react";
 
+const prompt =
+  "Write a response to this job opening in such a way that the client would want to hire me." +
+  "My skills: react, angular, vue. Have basic experience with python, django." +
+  "The response should be no more than 700 characters.";
+
 export const CopyButton = (props: { descr: string | null }) => {
   const [coppied, setCoppied] = useState(false);
+  const textToCopy = `${props.descr}. ${prompt}`;
 
   return (
     <Button
@@ -11,7 +17,7 @@ export const CopyButton = (props: { descr: string | null }) => {
       sx={{ my: 1 }}
       onClick={async () => {
         try {
-          await copyToClipboard(props.descr);
+          await copyToClipboard(textToCopy);
           setCoppied(true);
           setTimeout(() => setCoppied(false), 2000);
         } catch (e) {
