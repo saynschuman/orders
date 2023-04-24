ed82e5067dc69e01392d3934fa445e7c04f3b07c
vitaliy <vitaliy@heliotrope.digital>
2023-04-24 14:48:13 +0300

    limit fixed

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import { ProjectList } from "./types";
 export const getOrders = async () => {
   try {
     const res = await axios.get<ProjectList>(
-      "https://www.freelancer.com/api/projects/0.1/projects/active?limit=2&offset=0"
+      "https://www.freelancer.com/api/projects/0.1/projects/active?limit=1000&offset=0"
     );
     return res;
   } catch (e) {
