f23150b3e071179dcb8eb2f260680b75e9078384
vitaliy <vitaliy@heliotrope.digital>
2023-04-24 14:47:21 +0300

    render & styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { getOrders } from "./api";
+import { Box, Grid, Link, Typography } from "@mui/material";
 import { useQuery } from "react-query";
 
 function App() {
@@ -6,11 +7,28 @@ function App() {
   const orders = data?.data?.result?.projects;
 
   return (
-    <>
+    <Box sx={{ fontFamily: "sans-serif", p: 2 }}>
       {orders?.map((o) => (
-        <div key={o.id}>{o.id}</div>
+        <Box sx={{ mb: 2 }} key={o.id}>
+          <Typography sx={{ fontWeight: "bold" }}>{o.title} </Typography>
+          <Box>
+            <Grid container gap={1}>
+              <Grid item>{o.currency.code}</Grid>
+              <Grid item>{o.budget.maximum}</Grid>
+            </Grid>
+            <Box>
+              {o.preview_description}...
+              <Link
+                href={`https://www.freelancer.com/projects/${o.seo_url}`}
+                target="_blank"
+              >
+                (details)
+              </Link>
+            </Box>
+          </Box>
+        </Box>
       ))}
-    </>
+    </Box>
   );
 }
 
