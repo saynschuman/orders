f7d8381ec00edf4b0755e9875dc26f21a57eb61f
vitaliy <vitaliy@heliotrope.digital>
2023-04-25 22:55:07 +0300

    constants added

--- src/components/CopyButton/CopyButton.tsx
+++ src/components/CopyButton/CopyButton.tsx
@@ -1,14 +1,7 @@
+import { ABOUT, PROMPT } from "@/constants";
 import { copyToClipboard } from "@/utils";
 import { Button } from "@mui/material";
 import { useState } from "react";
-
-const prompt =
-  "Write a response to this job opening in such a way that the client would want to hire me." +
-  "My skills: react." +
-  "The response should be no more than 700 characters." +
-  "Please write response in two languages: english and russian";
-
-const about = "расскажи мне на русском языке, вкратце, о чем этот проект";
 
 export const CopyButton = (props: {
   descr: string | null;
@@ -16,7 +9,7 @@ export const CopyButton = (props: {
 }) => {
   const [coppied, setCoppied] = useState(false);
   const textToCopy = `${props.descr}. ${"\n"}${
-    props.type === "copy" ? prompt : about
+    props.type === "copy" ? PROMPT : ABOUT
   }`;
 
   return (
---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const QUERY = "react"
+
+export const PROMPT =
+  "Write a response to this job opening in such a way that the client would want to hire me." +
+  "My skills: react." +
+  "The response should be no more than 700 characters." +
+  "Please write response in two languages: english and russian";
+
+export const ABOUT = "расскажи мне на русском языке, вкратце, о чем этот проект";
