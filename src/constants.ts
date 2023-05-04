c9e3d23e50b4655cf8e8f9881e11245addf0a042
vitaliy <vitaliy@heliotrope.digital>
2023-05-04 23:38:32 +0300

    message fixed

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const QUERY = "react"
 export const PROMPT =
   "Write a response to this job opening in such a way that the client would want to hire me." +
   "My skills: react." +
-  "The response should be no more than 700 characters." +
+  "The response should be no more than 1500 characters." +
   "Please write response in two languages: english and russian";
 
 export const ABOUT = "расскажи мне на русском языке, вкратце, о чем этот проект";
